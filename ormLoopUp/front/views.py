64b67a1b0a0a68411b902e527fd231cd00a9e8b1
Null <546464268@qq.com>
2020-02-11 23:08:00 +0800

    contains & icontains

--- ormLoopUp/front/views.py
+++ ormLoopUp/front/views.py
@@ -14,4 +14,16 @@ def index(request):
     # FROM `front_article` WHERE `front_article`.`id` LIKE 1
     article1 = Article.objects.filter(id__iexact=1)
     print(article1.query)
+
+    # 会使用大小写敏感
+    # SELECT `front_article`.`id`, `front_article`.`title`, `front_article`.`content` FROM `front_article`
+    # WHERE `front_article`.`title` LIKE BINARY %zh9ngs%
+    article2 = Article.objects.filter(title__contains='zh9ngs')
+    print(article2.query)
+
+    # 忽略大小写
+    # SELECT `front_article`.`id`, `front_article`.`title`, `front_article`.`content` FROM `front_article`
+    # WHERE `front_article`.`title` LIKE %zh9ngs%
+    article3 = Article.objects.filter(title__icontains='zh9ngs')
+    print(article3.query)
     return HttpResponse("success")
